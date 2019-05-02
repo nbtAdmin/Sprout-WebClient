74ab2f94fac20a603271c034efa76801b3bbd5d0
okarim4259 <okarim4259@gmail.com>
2019-05-02 01:06:00 -0400

    Fixed Home component issue

--- webapp/src/app/components/core/home.component.ts
+++ webapp/src/app/components/core/home.component.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
   
+  public _showUseCase1: Boolean = false;
+  public _showUseCase2: Boolean = false;
+
   constructor() {}
 
   ngOnInit() {}
 
+  selectUseCase1() {
+    this._showUseCase1 = true;
+    this._showUseCase2 = false;
+  }
+
+  selectUseCase2() {
+    this._showUseCase2 = true;
+    this._showUseCase1 = false;
+  }
   
   
 }
